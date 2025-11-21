b1fd1aa9289b3b5200bb4f3f0124ba3ad0e12c1c
Zheniata <tataeva_evgeniia@inbox.ru>
2025-11-21 23:46:51 +0300

    lab4

--- src/main/java/Record/PlaceInfo.java
+++ src/main/java/Record/PlaceInfo.java
@@ -1,5 +1,11 @@
 package Record;
 
 public record PlaceInfo(String name, String size) {
-
+    @Override
+    public String toString() {
+        return "PlaceInfo{" +
+                "name='" + name + '\'' +
+                ", size='" + size + '\'' +
+                '}';
+    }
 }
